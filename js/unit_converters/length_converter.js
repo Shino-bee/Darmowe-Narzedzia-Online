96ca7083d0ed44dfa69316c6592adff37946a063
AceMichal <michal.podowski@gmail.com>
2023-06-12 10:54:45 +0200

    update length converter

--- js/unit_converters/length_converter.js
+++ js/unit_converters/length_converter.js
@@ -1,4 +1,50 @@
-export function length_converter() {
-  const test = "testowy tekst";
-  console.log(test);
+export function length_converter(indexValueInTable, whichValue, value) {
+  const lengthUnitValues = {
+    Metry: 1,
+    Kilometry: 0.001,
+    Centymetry: 100,
+    Milimetry: 1000,
+    Mikrometry: 1000000,
+    Nanometry: 1000000000,
+    Mile: 0.00062136887563302,
+    Stopy: 3.2808398950131,
+    Cale: 39.370078740157,
+    "Lata świetlne": 1.0570087076377e-16,
+  };
+  const baseLengthValue = lengthUnitValues[whichValue];
+  value = parseInt(value);
+  // console.log(indexValueInTable, whichValue, value, baseLengthValue);
+
+  switch (indexValueInTable) {
+    // meter
+    case 0:
+      return (value / baseLengthValue) * lengthUnitValues["Metry"];
+    // kilometer
+    case 1:
+      return (value / baseLengthValue) * lengthUnitValues["Kilometry"];
+    // centimeter
+    case 2:
+      return ((value / baseLengthValue) * lengthUnitValues["Centymetry"]);
+    // millimeter
+    case 3:
+      return (value / baseLengthValue) * lengthUnitValues["Milimetry"];
+    // micrometer
+    case 4:
+      return (value / baseLengthValue) * lengthUnitValues["Mikrometry"];
+    // nanometer
+    case 5:
+      return (value / baseLengthValue) * lengthUnitValues["Nanometry"];
+    // mile
+    case 6:
+      return (value / baseLengthValue) * lengthUnitValues["Mile"];
+    // foot
+    case 7:
+      return (value / baseLengthValue) * lengthUnitValues["Stopy"];
+    // inch
+    case 8:
+      return (value / baseLengthValue) * lengthUnitValues["Cale"];
+    // light year
+    case 9:
+      return (value / baseLengthValue) * lengthUnitValues["Lata świetlne"];
+  }
 }
--- js/unit_converters/main_unit_converters.js
+++ js/unit_converters/main_unit_converters.js
@@ -6,46 +6,69 @@ const page = path.split("/").pop();
 var unitCoverterFunction = () => {};
 
 if (page === "length_converter.html") {
-  unitCoverterFunction = length_converter();
+  unitCoverterFunction = length_converter;
 }
-console.log(unitCoverterFunction);
 
 // Changes the text in label of select tag to the selected value
 const unitToConvertOptions = document.getElementById("program-form-unit");
 unitToConvertOptions.addEventListener("change", () => {
-  const unitToConvertClass = document.getElementsByClassName("unit-to-convert");
+  const unitToConvertLabel =
+    document.getElementsByClassName("unit-to-convert")[0];
   const unitToConvertText =
     unitToConvertOptions.options[unitToConvertOptions.selectedIndex].text;
-  unitToConvertClass[0].innerText =
+  unitToConvertLabel.innerText =
     unitToConvertText.charAt(0).toLowerCase() + unitToConvertText.slice(1);
 });
 
 // Calculate button - calculates the results and changes content (text and results) in the table and shows table if is not displayed
 const unitConverBtnCalculate = document.getElementById(
-  "program-form-btn-converter"
+  "program-form-btn-calculate"
 );
 unitConverBtnCalculate.addEventListener("click", () => {
   const tableOfUnits = document.getElementById("program-table");
-  const unitToConvertClass = document.getElementsByClassName("unit-to-convert");
+  const unitToConvertTableNames =
+    document.getElementsByClassName("unit-to-convert");
+  const unitToConvertTableValues = document.getElementsByClassName(
+    "unit-value-converted"
+  );
   const unitToConvertText =
     unitToConvertOptions.options[unitToConvertOptions.selectedIndex].text;
+  const unitToConvertValue =
+    document.getElementById("program-form-value").value;
+  const programFormValidity = document
+    .getElementById("program-form")
+    .checkValidity();
 
-  for (let i = 1; i < unitToConvertClass.length; i++) {
-    unitToConvertClass[i].innerText = unitToConvertText;
-  }
-
-  if (tableOfUnits.style.opacity == 0) {
-    tableOfUnits.style.transition = "0.8s ease-out";
-    tableOfUnits.style.marginTop = "20px";
-    tableOfUnits.style.height = "auto";
-    tableOfUnits.style.opacity = 1;
+  if (programFormValidity === true) {
+    for (let i = 1; i < unitToConvertTableNames.length; i++) {
+      unitToConvertTableNames[i].innerText = unitToConvertText;
+    }
+    for (let i = 0; i < unitToConvertTableValues.length; i++) {
+      unitToConvertTableValues[i].innerText = unitCoverterFunction(
+        i,
+        unitToConvertText,
+        unitToConvertValue
+      );
+    }
+    if (tableOfUnits.style.opacity == 0) {
+      tableOfUnits.style.transition = "0.8s";
+      tableOfUnits.style.marginTop = "20px";
+      tableOfUnits.style.height = "auto";
+      tableOfUnits.style.opacity = 1;
+    }
   }
 });
 
 // Reset button - hide table
+const defaultUnitToConvertLabel =
+  document.getElementsByClassName("unit-to-convert")[0].textContent;
+console.log("xd:", defaultUnitToConvertLabel);
 const unitConverBtnReset = document.getElementById("program-form-btn-reset");
 unitConverBtnReset.addEventListener("click", () => {
+  const unitToConvertLabel =
+    document.getElementsByClassName("unit-to-convert")[0];
   const tableOfUnits = document.getElementById("program-table");
+  unitToConvertLabel.innerText = defaultUnitToConvertLabel;
   tableOfUnits.style.transition = "0s";
   tableOfUnits.style.marginTop = "0px";
   tableOfUnits.style.height = "0";
