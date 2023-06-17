b557a948e82676104527e60448053c572f1ca48d
AceMichal <michal.podowski@gmail.com>
2023-06-18 00:08:56 +0200

    create header template usage for every pages

---
+++ js/_header.js
@@ -0,0 +1,37 @@
+const menuDropdown = document.getElementById("header-nav-menu-dropdown");
+const menuDropdownBackground = document.getElementById(
+  "menu-dropdown-background"
+);
+const menuDropdownTopDefaultValue = getComputedStyle(menuDropdown).top;
+
+// Hide the menu navbar function
+function menuDropdownHide() {
+  menuDropdown.style.visibility = "hidden";
+  menuDropdown.style.opacity = 0;
+  menuDropdown.style.top = menuDropdownTopDefaultValue;
+  menuDropdownBackground.style.display = "none";
+}
+
+// Click on the menu navbar button (open or close)
+document.getElementById("header-nav-menu-btn").addEventListener("click", () => {
+  if (getComputedStyle(menuDropdown).visibility === "hidden") {
+    menuDropdown.style.visibility = "visible";
+    menuDropdown.style.opacity = 1;
+    menuDropdown.style.top = "52px";
+    menuDropdownBackground.style.display = "block";
+  } else {
+    menuDropdownHide();
+  }
+});
+
+// Click on the menu navbar links to hide menu navbar
+const menuDropdownItems = document.getElementsByClassName("menu-dropdown-a");
+for (let i = 0; i < menuDropdownItems.length; i++) {
+  const dropdownItem = menuDropdownItems[i];
+  dropdownItem.addEventListener("click", menuDropdownHide);
+}
+
+// Click anywhere (background, except menu navbar button and menu navbar links) to hide menu navbar (if menu navbar is open)
+document
+  .getElementById("menu-dropdown-background")
+  .addEventListener("click", menuDropdownHide);
--- js/main.js
+++ js/main.js
@@ -1,33 +1,4 @@
-// Hide the menu navbar
-function menuDropdownHide() {
-  const menu_dropdown = document.getElementById("header-nav-menu-dropdown");
-  const menu_dropdown_bg = document.getElementById("menu-dropdown-bg");
-  menu_dropdown.style.visibility = "";
-  menu_dropdown.style.opacity = 0;
-  menu_dropdown_bg.style.display = "";
-}
-
-// Click on the menu navbar button (open or close)
-document.getElementById("header-nav-menu-btn").addEventListener("click", () => {
-  const menu_dropdown = document.getElementById("header-nav-menu-dropdown");
-  const menu_dropdown_bg = document.getElementById("menu-dropdown-bg");
-  if (menu_dropdown.style.visibility === "") {
-    menu_dropdown.style.visibility = "visible";
-    menu_dropdown.style.opacity = 1;
-    menu_dropdown_bg.style.display = "block";
-  } else {
-    menuDropdownHide();
-  }
+// Load header navbar template from html file (template-header.html)
+$(function () {
+  $("#header").load("./html/_templates/template_header.html");
 });
-
-// If menu navbar is open click anywhere (background) except menu navbar button and menu navbar links to hide menu navbar
-document
-  .getElementById("menu-dropdown-bg")
-  .addEventListener("click", menuDropdownHide);
-
-// Click on the menu navbar links to hide menu navbar
-const menu_dropdown_items = document.getElementsByClassName("menu-dropdown-a");
-for (let i = 0; i < menu_dropdown_items.length; i++) {
-  const dropdown_item = menu_dropdown_items[i];
-  dropdown_item.addEventListener("click", menuDropdownHide);
-}
