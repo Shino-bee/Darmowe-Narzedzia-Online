8c33c3931e9361d0fb3f82b078ecf703229e1121
AceMichal <michal.podowski@gmail.com>
2023-06-18 21:43:06 +0200

    add and update footer template

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@
 $(function () {
   $("#header").load("./html/_templates/template_header.html");
 });
+
+// Load footer template from html file (template-footer.html)
+$(function () {
+  $("#footer").load("./html/_templates/template_footer.html");
+});
