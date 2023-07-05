f06b3b84cfc2f209a66d39f3616409e3d2bc5280
AceMichal <michal.podowski@gmail.com>
2023-07-05 07:59:24 +0200

    update pct calc, 2/5 form done, add responsive

--- js/online_calculators/percentage_calculator.js
+++ js/online_calculators/percentage_calculator.js
@@ -1,6 +1,5 @@
 const form1 = document.getElementById("form1");
 const form1Btn = document.getElementById("form1-btn");
-
 form1Btn.addEventListener("click", () => {
   if (form1.checkValidity() == true) {
     const form1WhatIsPct = parseFloat(
@@ -11,5 +10,20 @@ form1Btn.addEventListener("click", () => {
     );
     const form1Result = document.getElementById("form1-result");
     form1Result.value = (form1WhatIsPct / 100) * form1OfNum;
+  }
+});
+
+const form2 = document.getElementById("form2");
+const form2Btn = document.getElementById("form2-btn");
+form2Btn.addEventListener("click", () => {
+  if (form2.checkValidity() == true) {
+    const form2WhatPctOfNum = parseFloat(
+      document.getElementById("form2-what-pct-of-num").value
+    );
+    const form2IsNum = parseFloat(
+      document.getElementById("form2-is-num").value
+    );
+    const form2Result = document.getElementById("form2-result");
+    form2Result.value = (form2IsNum / form2WhatPctOfNum) * 100 + "%";
   }
 });
