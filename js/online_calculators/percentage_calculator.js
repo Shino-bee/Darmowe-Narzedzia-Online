213bf9bf1c805f494a0cd678472c8d111c2cd6d4
AceMichal <michal.podowski@gmail.com>
2023-07-05 10:44:19 +0200

    FINISHED pct calculator, 5/5 form done

--- js/online_calculators/percentage_calculator.js
+++ js/online_calculators/percentage_calculator.js
@@ -27,3 +27,46 @@ form2Btn.addEventListener("click", () => {
     form2Result.value = (form2IsNum / form2WhatPctOfNum) * 100 + "%";
   }
 });
+
+const form3 = document.getElementById("form3");
+const form3Btn = document.getElementById("form3-btn");
+form3Btn.addEventListener("click", () => {
+  if (form3.checkValidity() == true) {
+    const form3ByHowMuchPct = parseFloat(
+      document.getElementById("form3-by-how-much-pct").value
+    );
+    const form3IsDifferentFrom = parseFloat(
+      document.getElementById("form3-is-different-from").value
+    );
+    const form3Result = document.getElementById("form3-result");
+    form3Result.value =
+      ((form3IsDifferentFrom - form3ByHowMuchPct) / form3ByHowMuchPct) * 100 +
+      "%";
+  }
+});
+
+const form4 = document.getElementById("form4");
+const form4Btn = document.getElementById("form4-btn");
+form4Btn.addEventListener("click", () => {
+  if (form4.checkValidity() == true) {
+    const form4Num = parseFloat(document.getElementById("form4-num").value);
+    const form4IncreaseByPct = parseFloat(
+      document.getElementById("form4-increase-by-pct").value
+    );
+    const form4Result = document.getElementById("form4-result");
+    form4Result.value = form4Num * (1 + form4IncreaseByPct / 100);
+  }
+});
+
+const form5 = document.getElementById("form5");
+const form5Btn = document.getElementById("form5-btn");
+form5Btn.addEventListener("click", () => {
+  if (form5.checkValidity() == true) {
+    const form5Num = parseFloat(document.getElementById("form5-num").value);
+    const form5ReduceByPct = parseFloat(
+      document.getElementById("form5-reduce-by-pct").value
+    );
+    const form5Result = document.getElementById("form5-result");
+    form5Result.value = form5Num * (1 - form5ReduceByPct / 100);
+  }
+});
