dca362c5abe085724d9bca179474a2e785806dfe
AceMichal <michal.podowski@gmail.com>
2023-07-06 11:18:09 +0200

    add average calculator, remove jquery from project

--- js/_main.js
+++ js/_main.js
@@ -1,9 +1,15 @@
-// Load header navbar template from html file (template-header.html)
-$(function () {
-  $("#header").load("../../pages/_templates/template_header.html");
-});
+// Load header navbar template from html file (template_header.html)
+const xhrHeader = new XMLHttpRequest();
+xhrHeader.onload = function () {
+  document.getElementById("header").innerHTML = this.response;
+};
+xhrHeader.open("GET", "../../pages/_templates/template_header.html", false);
+xhrHeader.send();
 
-// Load footer template from html file (template-footer.html)
-$(function () {
-  $("#footer").load("../../pages/_templates/template_footer.html");
-});
+// Load footer template from html file (template_footer.html)
+const xhrFooter = new XMLHttpRequest();
+xhrFooter.onload = function () {
+  document.getElementById("footer").innerHTML = this.response;
+};
+xhrFooter.open("GET", "../../pages/_templates/template_footer.html", false);
+xhrFooter.send();
