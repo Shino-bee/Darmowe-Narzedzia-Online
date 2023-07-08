735ceaa8cc2504c11a1f62b763649465e86f2243
AceMichal <michal.podowski@gmail.com>
2023-07-08 16:20:50 +0200

    average calc page, 1/3 done (arithmethic average)

--- js/online_calculators/average_calculator.js
+++ js/online_calculators/average_calculator.js
@@ -1,9 +1,46 @@
-// Arithmetic average calculator
+/* Arithmetic average calculator */
+const arithmeticAverageBtnAdd = document.getElementById("arithmetic-average-btn-calculate");
+let arrOfNums = [];
+
+// Add button - calculates and shows added nums and results
+arithmeticAverageBtnAdd.addEventListener("click", () => {
+  const arithmeticAverageForm = document.getElementById("arithmetic-average-form");
+  const arithmeticAverageInput = document.getElementById("arithmetic-average-input");
+  if (arithmeticAverageForm.checkValidity() && arithmeticAverageInput.value != "") {
+    const arithmeticAverageAddedNums = document.getElementById("arithmetic-average-added-nums");
+    console.log(arithmeticAverageAddedNums.style.display);
+    if (
+      arithmeticAverageAddedNums.style.display == "" ||
+      arithmeticAverageAddedNums.style.display == "none"
+    ) {
+      arithmeticAverageAddedNums.style.display = "block";
+      arithmeticAverageAddedNums.innerText += arithmeticAverageInput.value;
+    } else {
+      arithmeticAverageAddedNums.innerText += ", " + arithmeticAverageInput.value;
+    }
+    arrOfNums.push(parseFloat(arithmeticAverageInput.value));
+    const arithmeticAverageOutput = document.getElementById("arithmetic-average-result");
+    const arithmeticAverageResult = (
+      arrOfNums.reduce((sum, num) => sum + num) / arrOfNums.length
+    ).toFixed(2);
+    arithmeticAverageOutput.value = arithmeticAverageResult;
+    arithmeticAverageInput.value = "";
+  }
+
+  // Reset button
+  const arithmeticAverageBtnReset = document.getElementById("arithmetic-average-btn-reset");
+  arithmeticAverageBtnReset.addEventListener("click", () => {
+    const arithmeticAverageAddedNums = document.getElementById("arithmetic-average-added-nums");
+    arithmeticAverageAddedNums.innerText = "";
+    arithmeticAverageAddedNums.style.display = "none";
+  });
+});
+
+/* const invalidChars = /[^0-9,]/gi;
 const arithmeticAverageInput = document.getElementById(
   "arithmetic-average-input"
 );
 arithmeticAverageInput.addEventListener("keyup", () => {
-  const invalidChars = /[^0-9,]/gi;
   if (invalidChars.test(arithmeticAverageInput.value)) {
     arithmeticAverageInput.value = arithmeticAverageInput.value.replace(
       invalidChars,
@@ -20,6 +57,19 @@ arithmeticAverageBtnCalculate.addEventListener("click", () => {
     "arithmetic-average-form"
   );
   if (arithmeticAverageForm.checkValidity() == true) {
-    // pass
+    let arrOfNums = arithmeticAverageInput.value
+      .split(",")
+      .filter((num) => num.length > 0);
+    for (let i = 0; i < arrOfNums.length; i++) {
+      if (invalidChars.test(arrOfNums[i])) {
+        arrOfNums[i] = arrOfNums[i].replace(invalidChars, "");
+        if (arrOfNums[i].length <= 0) {
+          arrOfNums.splice(i, 1);
+        }
+      }
+    }
+
+    console.log(arrOfNums);
+    console.log(typeof(arrOfNums[0]));
   }
-});
+}); */
