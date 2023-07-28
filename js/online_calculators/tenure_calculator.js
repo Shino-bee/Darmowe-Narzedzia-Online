68bd1aa49016ec03467c7c7a74a642218a683c68
Michał Podowski <michal.podowski@gmail.com>
2023-07-28 10:35:18 +0200

    FINISHD tenure calculator

--- js/online_calculators/tenure_calculator.js
+++ js/online_calculators/tenure_calculator.js
@@ -1,14 +1,14 @@
 const startingDateId = document.getElementById("tenure-calculator-input1");
 const endingDateId = document.getElementById("tenure-calculator-input2");
 const tenureCalculatorStartingLabel = document.getElementById("tenure-calculator-starting-label");
-let tenureTableOfResults = [0, 0, 0]; // years, months, days
+let tenureArrayOfResults = [0, 0, 0]; // years, months, days
 
 // Hide the table, reset to default values
 function reset() {
   const tenureAddedDates = document.getElementById("tenure-calculator-added-dates");
   tenureAddedDates.innerText = "";
   tenureAddedDates.style.display = "none";
-  tenureTableOfResults = [0, 0, 0];
+  tenureArrayOfResults = [0, 0, 0];
   if (startingDateId.style.backgroundColor === "var(--main-color-red)") {
     startingDateId.style.backgroundColor = "var(--main-color-darklight)";
     tenureCalculatorStartingLabel.style.backgroundColor = "";
@@ -17,39 +17,40 @@ function reset() {
 }
 reset();
 
+// Hide error status if changed startingDate is less than endingDate
 startingDateId.addEventListener("change", () => {
-  let startingDate = new Date(startingDateId.valueAsDate);
-  let endingDate = new Date(endingDateId.valueAsDate);
-  if (startingDateId.style.backgroundColor === "var(--main-color-red)" && Math.sign(endingDate - startingDate) > 0) {
+  const startingDate = new Date(startingDateId.valueAsDate);
+  const endingDate = new Date(endingDateId.valueAsDate);
+  if (
+    startingDateId.style.backgroundColor === "var(--main-color-red)" &&
+    Math.sign(endingDate - startingDate) > 0
+  ) {
     startingDateId.style.backgroundColor = "var(--main-color-darklight)";
     tenureCalculatorStartingLabel.style.backgroundColor = "";
     tenureCalculatorStartingLabel.innerText = "Data rozpoczęcia pracy:";
   }
 });
 
-const tenureArrOfNums = [];
 // Add button - calculates and shows added dates and results
 const tenureBtnAdd = document.getElementById("tenure-calculator-btn-calculate");
 tenureBtnAdd.addEventListener("click", () => {
   const tenureForm = document.getElementById("tenure-calculator-form");
   if (tenureForm.checkValidity()) {
-    let startingDate = new Date(startingDateId.valueAsDate);
-    let endingDate = new Date(endingDateId.valueAsDate);
+    const startingDate = new Date(startingDateId.valueAsDate);
+    const endingDate = new Date(endingDateId.valueAsDate);
+
+    // Show error status if startingDate is higher than endingDate
     if (Math.sign(endingDate - startingDate) <= 0) {
       startingDateId.style.backgroundColor = "var(--main-color-red)";
       tenureCalculatorStartingLabel.style.backgroundColor = "var(--main-color-red)";
-      tenureCalculatorStartingLabel.innerText = "Data rozpoczęcia musi być mniejsza niż data zakończenia!";
+      tenureCalculatorStartingLabel.innerText =
+        "Data rozpoczęcia musi być mniejsza niż data zakończenia!";
     } else {
-      if (startingDate > endingDate) {
-        const swap = startingDate;
-        startingDate = endingDate;
-        endingDate = swap;
-      }
-      const birthYear = startingDate.getFullYear();
+      const startingYear = startingDate.getFullYear();
       const february =
-        (birthYear % 4 === 0 && birthYear % 100 !== 0) || birthYear % 400 === 0 ? 29 : 28;
+        (startingYear % 4 === 0 && startingYear % 100 !== 0) || startingYear % 400 === 0 ? 29 : 28;
       const daysInMonth = [31, february, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-      let yearDiff = endingDate.getFullYear() - birthYear;
+      let yearDiff = endingDate.getFullYear() - startingYear;
       let monthDiff = endingDate.getMonth() - startingDate.getMonth();
       if (monthDiff < 0) {
         yearDiff--;
@@ -66,34 +67,39 @@ tenureBtnAdd.addEventListener("click", () => {
         dayDiff += daysInMonth[startingDate.getMonth()];
       }
 
+      // Update array of results (years, months, days) and print values
+      tenureArrayOfResults[0] += yearDiff;
+      tenureArrayOfResults[1] += monthDiff;
+      tenureArrayOfResults[2] += dayDiff;
+      if (tenureArrayOfResults[2] >= 31) {
+        tenureArrayOfResults[2] -= 31;
+        tenureArrayOfResults[1] += 1;
+      }
+      if (tenureArrayOfResults[1] >= 12) {
+        tenureArrayOfResults[1] -= 12;
+        tenureArrayOfResults[0] += 1;
+      }
       const tenureOutputResult = document.getElementById("tenure-calculator-result");
-      tenureTableOfResults[0] += yearDiff;
-      tenureTableOfResults[1] += monthDiff;
-      tenureTableOfResults[2] += dayDiff;
       tenureOutputResult.value =
-        tenureTableOfResults[0] +
+        tenureArrayOfResults[0] +
         " lat, " +
-        tenureTableOfResults[1] +
+        tenureArrayOfResults[1] +
         " miesięcy, " +
-        tenureTableOfResults[2] +
+        tenureArrayOfResults[2] +
         " dni";
 
+      // Print added dates (startingDate, endingDate) and differene between them
       const tenureAddedDates = document.getElementById("tenure-calculator-added-dates");
+      const tenureAddedDatesContent = `od ${startingDate.getDate()}.${
+        startingDate.getMonth() + 1
+      }.${startingDate.getFullYear()} do ${endingDate.getDate()}.${
+        endingDate.getMonth() + 1
+      }.${endingDate.getFullYear()} to ${yearDiff} lat, ${monthDiff} miesięcy, ${dayDiff} dni`;
       if (tenureAddedDates.style.display != "block") {
         tenureAddedDates.style.display = "block";
-        tenureAddedDates.innerText = `${endingDate.getDate()}.${
-          endingDate.getMonth() + 1
-        }.${endingDate.getFullYear()} - ${startingDate.getDate()}.${
-          startingDate.getMonth() + 1
-        }.${startingDate.getFullYear()} = ${yearDiff} lat, ${monthDiff} miesięcy, ${dayDiff} dni`;
+        tenureAddedDates.innerText = tenureAddedDatesContent;
       } else {
-        tenureAddedDates.innerText +=
-          "\n" +
-          `${endingDate.getDate()}.${
-            endingDate.getMonth() + 1
-          }.${endingDate.getFullYear()} - ${startingDate.getDate()}.${
-            startingDate.getMonth() + 1
-          }.${startingDate.getFullYear()} = ${yearDiff} lat, ${monthDiff} miesięcy, ${dayDiff} dni`;
+        tenureAddedDates.innerText += "\n" + tenureAddedDatesContent;
       }
     }
   }
