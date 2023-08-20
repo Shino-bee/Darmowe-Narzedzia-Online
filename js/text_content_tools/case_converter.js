89ef99303f5401d097a990930dd00277e83f066d
Michał Podowski <michal.podowski@gmail.com>
2023-08-20 14:01:43 +0200

    FINISHED case converter page

--- js/text_content_tools/case_converter.js
+++ js/text_content_tools/case_converter.js
@@ -1,36 +1,28 @@
-// const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-
 // Execute button - changes the text and displays it
 const programFormBtnExecute = document.getElementsByClassName("program-form-btn-calculate")[0];
 programFormBtnExecute.addEventListener("click", () => {
   const radioBtns = document.querySelectorAll('input[name="caseFormat"]');
   let radioBtnChecked = "";
-
+  // Check which radio option is selected
   for (let i = 0; i < radioBtns.length; i++) {
     if (radioBtns[i].checked) {
       radioBtnChecked = radioBtns[i].value;
       break;
     }
   }
-
   const inputTextarea = document.getElementById("input-textarea").value;
   let changedText = "";
+  // Changes and displays the text depending on the selected radio option
   switch (radioBtnChecked) {
     case "sentence case":
-      let itIsDot = false;
-      let itisFirstChar = true;
+      let itIsDot = true;
       for (let i = 0; i < inputTextarea.length; i++) {
         const char = inputTextarea[i];
-        if (itisFirstChar && char != " ") {
-          changedText += char.toUpperCase();
-          itisFirstChar = false;
-          continue;
-        }
-        if (char === ".") {
+        if (char === "." || char === "?" || char === "!") {
           itIsDot = true;
           changedText += char;
         } else {
-          if (itIsDot && char != " ") {
+          if (itIsDot && char !== " " && char !== "\n" && char !== "\r") {
             changedText += char.toUpperCase();
             itIsDot = false;
           } else {
@@ -39,66 +31,61 @@ programFormBtnExecute.addEventListener("click", () => {
         }
       }
       break;
+
     case "lower case":
       changedText = inputTextarea.toLowerCase();
       break;
+
     case "upper case":
       changedText = inputTextarea.toUpperCase();
       break;
+
     case "capitalized case":
-      console.log(radioBtnChecked);
+      let itIsSpace = true;
+      for (let i = 0; i < inputTextarea.length; i++) {
+        const char = inputTextarea[i];
+        if (char === " " || char === "\n" || char === "\r") {
+          itIsSpace = true;
+          changedText += char;
+        } else {
+          if (itIsSpace && char !== " " && char !== "\n" && char !== "\r") {
+            changedText += char.toUpperCase();
+            itIsSpace = false;
+          } else {
+            changedText += char.toLowerCase();
+          }
+        }
+      }
       break;
+
     case "inverse case":
-      console.log(radioBtnChecked);
+      let upperOrLower = "upper";
+      for (let i = 0; i < inputTextarea.length; i++) {
+        const char = inputTextarea[i];
+        if (upperOrLower === "upper") {
+          changedText += char.toUpperCase();
+          upperOrLower = "lower";
+        } else if (upperOrLower === "lower") {
+          changedText += char.toLowerCase();
+          upperOrLower = "upper";
+        }
+      }
       break;
   }
-  console.log("--------");
-  console.log(changedText);
-});
-
-// Clear button - removes text and set default height of input and output textareas
-const programFormBtnClean = document.getElementsByClassName("program-form-btn-reset")[0];
-
-/* // Execute button - changes the text and displays it
-const programFormBtnExecute = document.getElementsByClassName("program-form-btn-calculate")[0];
-cutprogramFormBtnExee.addEventListener("click", () => {
-  const inputRot13 = document.getElementById("input-rot13").value;
-  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-  let changedText = "";
-  for (let i = 0; i < inputRot13.length; i++) {
-    const char = inputRot13[i];
-    if (char === "\n") {
-      changedText += char;
-      continue;
-    }
-    if (alphabet.includes(char.toLowerCase())) {
-      let index = alphabet.indexOf(char.toLowerCase()) + 13;
-      if (index >= 26) {
-        index -= 26;
-      }
-      if (char === char.toUpperCase()) {
-        changedText += alphabet[index].toUpperCase();
-      } else {
-        changedText += alphabet[index];
-      }
-    } else {
-      changedText += char;
-    }
-  }
-  const outputRot13 = document.getElementById("output-rot13");
-  outputRot13.value = changedText;
-  outputRot13.style.height = outputRot13.scrollHeight + 2 + "px";
+  const outputTextarea = document.getElementById("output-textarea");
+  outputTextarea.value = changedText;
+  outputTextarea.style.height = outputTextarea.scrollHeight + 2 + "px";
 });
 
 // Clear button - removes text and set default height of input and output textareas
 const programFormBtnClean = document.getElementsByClassName("program-form-btn-reset")[0];
 programFormBtnClean.addEventListener("click", () => {
-  const inputRot13 = document.getElementById("input-rot13");
-  const outputRot13 = document.getElementById("output-rot13");
-  inputRot13.value = "";
-  inputRot13.rows = "12";
-  inputRot13.style.height = "auto";
-  outputRot13.value = "";
-  outputRot13.rows = "8";
-  outputRot13.style.height = "auto";
-}); */
+  const inputTextarea = document.getElementById("input-textarea");
+  const outputTextarea = document.getElementById("output-textarea");
+  inputTextarea.value = "";
+  inputTextarea.rows = "12";
+  inputTextarea.style.height = "auto";
+  outputTextarea.value = "";
+  outputTextarea.rows = "8";
+  outputTextarea.style.height = "auto";
+});
