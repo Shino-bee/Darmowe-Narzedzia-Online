16a92f9acc2c7dd0a60a09c5b4c99bcdae465627
Michał Podowski <michal.podowski@gmail.com>
2024-02-20 11:59:58 +0100

    UPDATE word counter page, new concept for word and sentence count

--- js/text_content_tools/word_counter.js
+++ js/text_content_tools/word_counter.js
@@ -1,6 +1,5 @@
 const Alphabet = "AaĄąBbCcĆćDdEeĘęFfGgHhIiJjKkLlŁłMmNnŃńOoÓóPpQqRrSsŚśTtUuVvWwXxYyZzŹźŻż".split("");
 const tableOfResults = document.getElementById("program-table");
-tableOfResults.style;
 
 // Execute button - counts the text content and displays it in a table
 const programFormBtnExecute = document.getElementsByClassName("program-form-btn-calculate")[0];
@@ -20,83 +19,113 @@ programFormBtnExecute.addEventListener("click", () => {
       Paragraphs: 0,
     };
 
-    let isSpace = true;
+    let isSpace = false;
     let isWord = false;
-    let isSentence = true;
-    let isNewLine = true;
+    let isEndPunctuation = false;
+    let isNewLine = false;
     const endPunctuationMarks = [".", "!", "?"];
     for (let i = 0; i < inputTextarea.length; i++) {
       const char = inputTextarea[i];
 
+      if (isWord && (isSpace || isNewLine || i === inputTextarea.length - 1)) {
+        console.log("SŁOWO", char === "\n");
+        if (char !== " " && char !== "\n") {
+          wordCounterResults["Words"]++;
+          isWord = false;
+          isSpace = false;
+        } else console.log("space");
+      }
+
+      if (isNewLine || i === inputTextarea.length - 1) {
+        wordCounterResults["Paragraphs"]++;
+        isNewLine = false;
+      }
+
+      // --------------------------------------------
+
       // Add a point if there is WORD (e.g. hello)
-      if (isWord && (isSpace || isNewLine)) {
-        console.log("SŁOWO");
-        wordCounterResults["Words"]++;
-        isWord = false;
-        isSpace = false;
-      }
+      // if (isWord && (isSpace || isNewLine)) {
+      //   console.log("SŁOWO");
+      //   wordCounterResults["Words"]++;
+      //   isWord = false;
+      //   isSpace = false;
+
+      // if (i === inputTextarea.length - 1) {
+      //   console.log("KONIEC");
+      //   wordCounterResults["Sentences"]++;
+      // }
+      // }
+
       // Add a point if there is NEW LINE (e.g. \n, enter)
-      if (isNewLine) {
-        if (char === "\n") continue;
-        else {
-          wordCounterResults["Paragraphs"]++;
-          isNewLine = false;
-        }
-      }
+      // if (isNewLine) {
+      //   console.log("NEW LINE:", char);
+      //   if (char === "\n") continue;
+      //   else {
+      //     wordCounterResults["Paragraphs"]++;
+      //     isNewLine = false;
+      //   }
+      // }
+
       // Add a point if there is correct SENTENCE (e.g. Hello world!)
-      if (isSentence) {
-        const nextChar = inputTextarea[i + 1];
-        console.log("ZDANIE", nextChar);
-        if (nextChar === undefined || nextChar === " ") {
-          wordCounterResults["Spaces"]++;
-          wordCounterResults["All chars"]++;
-          continue;
-        } else if (nextChar === "\n") continue;
-        if (
-          i === 0 ||
-          ((char === " " || char === "\n") &&
-            ((Alphabet.includes(nextChar) && nextChar === nextChar.toUpperCase()) ||
-              nextChar.match(/[0-9]/)))
-        ) {
-          console.log("JEST");
-          wordCounterResults["Sentences"]++;
-        }
-        isSentence = false;
-      }
+      // if (isEndPunctuation) {
+      //   console.log("ZDANIE");
+
+      //   const nextChar = inputTextarea[i + 1];
+      //   console.log(nextChar);
+      //   // Add a point to SENTENCE at start or end
+      //   if (i === 0 || nextChar === undefined) {
+      //     wordCounterResults["Sentences"]++;
+      //     isEndPunctuation = false;
+      //     continue;
+      //   }
+
+      //   if (char === " " || char === "\n") {
+      //     if (
+      //       (Alphabet.includes(nextChar) && nextChar === nextChar.toUpperCase()) ||
+      //       nextChar.match(/[0-9]/)
+      //     ) {
+      //       console.log("+ ZDANIE +");
+      //       wordCounterResults["Sentences"]++;
+      //       isEndPunctuation = false;
+      //     }
+      //   }
+      // }
+
+      // -------------------------------------------------
 
       // Add a point to ALL CHARS
       wordCounterResults["All chars"]++;
       // Add a point if char is LETTER of the Polish alphabet (AaĄąBbCcĆć...)
       if (Alphabet.includes(char)) {
-        console.log("LITERA:", char);
+        // console.log("LITERA:", char);
         wordCounterResults["Letters"]++;
         isWord = true;
       } // Add a point if char is NUMBER (123...)
       else if (char.match(/[0-9]/)) {
-        console.log("CYFRA:", char);
+        // console.log("CYFRA:", char);
         wordCounterResults["Numbers"]++;
         isWord = true;
       } // Add a point if char is SPACE ( )
       else if (char === " ") {
-        console.log("SPACJA:", char);
+        // console.log("SPACJA:", char);
         wordCounterResults["Spaces"]++;
         isSpace = true;
       } // Reduce a point of ALL CHARS if char is NEW LINE (\n, enter)
       else if (char === "\n") {
-        console.log("NEW LINE:", char);
+        // console.log("NEW LINE:", char);
         wordCounterResults["All chars"]--;
         isNewLine = true;
       } // Add a point if char is END PUNCTUATION MARK (.!?)
       else if (endPunctuationMarks.includes(char)) {
         console.log("INTERPUNKCJA:", char);
         wordCounterResults["Special chars"]++;
-        isWord = true;
-        isSentence = true;
+        // isWord = true;
+        isEndPunctuation = true;
       } // Add a point if char is SPECIAL CHARACTER (!@#$%^&...)
       else {
-        console.log("SPECJALNY", char);
+        // console.log("SPECJALNY", char);
         wordCounterResults["Special chars"]++;
-        isWord = true;
+        // isWord = true;
       }
     }
 
@@ -105,7 +134,6 @@ programFormBtnExecute.addEventListener("click", () => {
     for (let i = 0; i < tableOfValues.length; i++) {
       const result = Object.values(wordCounterResults)[i];
       const tableCellValue = tableOfValues[i];
-      console.log(result);
       tableCellValue.innerText = result;
     }
   }
