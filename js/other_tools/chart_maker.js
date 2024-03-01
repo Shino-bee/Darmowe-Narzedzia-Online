87b414d63aaa314eeec7e511468b60fcaa51e3fa
Michał Podowski <michal.podowski@gmail.com>
2024-03-01 21:47:24 +0100

    UPDATE chart maker, improve comments, improve buttons operations

--- js/other_tools/chart_maker.js
+++ js/other_tools/chart_maker.js
@@ -1,4 +1,6 @@
-// Function updates selected chart depeding on selected chart or button pressed
+// -----------------------------------------------
+// ------------------ FUNCTIONS ------------------
+/* Function updates selected chart depeding on selected chart or button pressed (show values on selected chart) */
 function updateChart(chart) {
   switch (chart) {
     case "pie chart":
@@ -13,27 +15,30 @@ function updateChart(chart) {
   }
 }
 
-// Function updates functionality for dynamic display values in chart while user entered input values (label and data)
+/* Function updates/adds functionality for dynamic display values in chart while user entered input values (label and data) */
 function updateInputs() {
+  // Data's labels - change value in chart when user types text in input (dynamically)
   for (let i = 0; i < inputLabels.length; i++) {
-    // Data's labels - change value in chart when user types input (dynamically)
     inputLabels[i].addEventListener("input", () => {
       chartLabels[i] = inputLabels[i].value;
       updateChart(selectedChart);
     });
-
-    // Data's values - change value in chart when user types input (dynamically)
-    inputData[i].addEventListener("input", () => {
-      console.log(inputData[i].value);
-      console.log(inputsContainer);
-      chartData[chartData.length - 1][i] = parseFloat(inputData[i].value);
-      updateChart(selectedChart);
-    });
+  }
+  // Data's values - change value in chart when user types number in input (dynamically)
+  for (let i = 0; i < chartDatasetsAmount; i++) {
+    for (let j = 0; j < inputs.length; j++) {
+      inputs[j].getElementsByTagName("input")[i + 1].addEventListener("input", () => {
+        const inputValue = parseFloat(inputs[j].getElementsByTagName("input")[i + 1].value);
+        chartData[i][j] = inputValue;
+        updateChart(selectedChart);
+      });
+    }
   }
 }
 
-// -----------GLOBAL VARIABLES----------
-// USER DATA initialize
+// ------------------------------------------------
+// --------------- GLOBAL VARIABLES ---------------
+/* USER DATA initialize */
 const chartLabels = ["Czerwony", "Niebieski", "Żółty", "Pomarańczowy", "Zielony", "Fioletowy"];
 const chartData = [[65, 59, 70, 81, 60, 55]];
 
@@ -45,19 +50,18 @@ const chartDatasets = [
   },
 ];
 
-// Initialize INPUT LABELS AND TAGS and adds default values
+/* Initialize INPUT LABELS AND TAGS and adds default values */
 const inputsContainer = document.getElementById("data-inputs-container");
 const inputs = document.getElementsByClassName("data-inputs");
 const inputLabels = document.getElementsByClassName("labels");
 const inputData = document.getElementsByClassName("data");
 for (let i = 0; i < inputLabels.length; i++) {
-  const label = inputLabels[i];
-  const data = inputData[i];
-  label.value = chartLabels[i];
-  data.value = chartData[0][i];
+  inputLabels[i].value = chartLabels[i];
+  inputData[i].value = chartData[0][i];
+  inputs[i].style.gridTemplateColumns = "0.5fr 1fr";
 }
 
-// Default COLORS for chart elements
+/* Default COLORS for chart elements */
 const backgroundColors = [
   "rgba(255, 99, 132, 0.5)",
   "rgba(54, 162, 235, 0.5)",
@@ -93,8 +97,9 @@ const borderColors = [
   "rgb(127, 194, 142)",
 ];
 
-// -------------------CHARTS---------------------------
-// Pie Chart
+// ------------------------------------------------
+// ------------------- CHARTS ---------------------
+/* Pie Chart */
 const pieChartId = document.getElementById("pie-chart");
 
 const pieChart = new Chart(pieChartId, {
@@ -117,7 +122,7 @@ const pieChart = new Chart(pieChartId, {
   },
 });
 
-// Bar Chart
+/* Bar Chart */
 const barChartId = document.getElementById("bar-chart");
 
 const barChart = new Chart(barChartId, {
@@ -146,7 +151,7 @@ const barChart = new Chart(barChartId, {
   },
 });
 
-// Line Chart
+/* Line Chart */
 const lineChartId = document.getElementById("line-chart");
 
 const lineChart = new Chart(lineChartId, {
@@ -176,12 +181,12 @@ const lineChart = new Chart(lineChartId, {
   },
 });
 
-// Initialize functionality
+/* Initialize functionality for inputs */
 updateInputs();
 
-// -------------------------------------------------
-// ---------------BUTTONS & SELECT------------------
-// Select chart - display selected option and hide the rest
+// ------------------------------------------------
+// ---------------- BUTTONS & SELECT --------------
+/* Select chart - display selected option, hide the rest and updates chart */
 const selectChart = document.getElementById("select-chart");
 let selectedChart = selectChart[selectChart.selectedIndex].value;
 selectChart.addEventListener("change", () => {
@@ -205,41 +210,55 @@ selectChart.addEventListener("change", () => {
   updateChart(selectedChart);
 });
 
-// "Add Dataset" button - adds to chart new dataset (copies last data array), his label and inputs
+/* "Add Dataset" button */
 const addDatasetButton = document.getElementById("add-dataset-btn");
 addDatasetButton.addEventListener("click", () => {
-  console.log(chartData);
   chartDatasetsAmount++;
-  chartData.push(chartData[chartData.length - 1]);
+  chartData.push([]);
+  for (let i = 0; i < inputs.length; i++) {
+    // Copies values from last dataset array to new pushed empty array
+    const valueToCopy = parseFloat(inputs[i].lastElementChild.value);
+    chartData[chartData.length - 1].push(valueToCopy);
+    // Creates new input tag with copied value
+    const inputTag = `<input type="number" name="data" class="data" value="${valueToCopy}"/>`;
+    inputs[i].insertAdjacentHTML("beforeend", inputTag);
+    // Change inputs container grid-template-column value from "0.5fr 1fr" to "1fr 1fr 1fr"
+    const inputsGridTemplateColumns = inputs[i].style.gridTemplateColumns;
+    if (inputsGridTemplateColumns !== "1fr 1fr 1fr") {
+      inputs[i].style.gridTemplateColumns = "1fr 1fr 1fr";
+    }
+  }
+  // Pushes to chart new dataset with label and copied values
   chartDatasets.push({
     label: ` Zbiór danych ${chartDatasetsAmount}`,
     data: chartData[chartDatasetsAmount - 1],
   });
-
-  for (let i = 0; i < inputs.length; i++) {
-    const inputTag = `<input type="number" name="data" class="data" value="${
-      chartData[chartData.length - 1][i]
-    }"/>`;
-    inputs[i].style.gridTemplateColumns = "1fr 1fr 1fr";
-    inputs[i].insertAdjacentHTML("beforeend", inputTag);
-  }
+  // Update chart and inputs
   updateChart(selectedChart);
   updateInputs();
 });
 
-// "Remove dataset" button - removes last dataset, his label and inputs
+/* "Remove dataset" button - removes last dataset, his label and inputs */
 const removeDatasetButton = document.getElementById("remove-dataset-btn");
 removeDatasetButton.addEventListener("click", () => {
+  // Remove
   if (chartDatasetsAmount > 1) {
     chartDatasetsAmount--;
     chartData.pop();
     chartDatasets.pop();
-    for (let i = 0; i < inputs.length; i++) inputs[i].removeChild(inputs[i].lastElementChild);
+
+    for (let i = 0; i < inputs.length; i++) {
+      if (chartDatasetsAmount <= 1) {
+        inputs[i].style.gridTemplateColumns = "0.5fr 1fr";
+      }
+      const lastDatasetInputs = inputs[i].lastElementChild;
+      inputs[i].removeChild(lastDatasetInputs);
+    }
     updateChart(selectedChart);
   }
 });
 
-// "Add Data" button - adds to chart new data (copies last data value of last array), his label (with box-shadow color) and inputs
+/* "Add Data" button - adds to chart new data (copies last data value of last array), his label (with box-shadow color) and inputs */
 const addDataButton = document.getElementById("add-data-btn");
 addDataButton.addEventListener("click", () => {
   const data = chartData[chartData.length - 1][chartData[chartData.length - 1].length - 1];
@@ -247,22 +266,29 @@ addDataButton.addEventListener("click", () => {
     chartLabels.length >= borderColors.length
       ? borderColors[chartLabels.length % borderColors.length]
       : borderColors[chartLabels.length];
-  // console.log(labelBoxShadow);
-  // console.log(chartLabels.length, );
-  // if (chartLabels.length <= borderColors.length) borderColors[borderColors.length];
-  // else false;
+  // $$$$$$$$$$ CREATE INPUTS NUMBER DEPENDING ON chartDatasetsAmount
   const inputTag = `<div class="data-inputs">
   <input type="text" name="labels" class="labels" autocomplete="off" value="Nowy" style="box-shadow: 0px 0px 10px ${labelBoxShadow};"/>
   <input type="number" name="data" class="data" value="${data}"/>
 </div>`;
   chartLabels.push("Nowy");
-  chartData[0].push(data);
+  // $$$$$$$$$$$
+  console.log(data);
   inputsContainer.insertAdjacentHTML("beforeend", inputTag);
+  // $$$$$$$$$$$
+  for (let i = 0; i < chartDatasetsAmount - 1; i++) {
+    console.log(inputs[inputs.length - 2]);
+    inputs[inputs.length - 1].insertAdjacentHTML(
+      "beforeend",
+      `<input type="number" name="data" class="data" value="${data}"/>`
+    );
+  }
+
   updateChart(selectedChart);
   updateInputs();
 });
 
-// "Remove data" button - removes last data, his label and inputs
+/* "Remove data" button - removes last data, his label and inputs */
 const removeDataButton = document.getElementById("remove-data-btn");
 removeDataButton.addEventListener("click", () => {
   if (chartData[chartData.length - 1].length > 1) {
