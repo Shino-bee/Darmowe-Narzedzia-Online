6a123778909f03e714e7b7c4d2e77790023091a7
Michał Podowski <michal.podowski@gmail.com>
2024-03-10 00:57:07 +0100

    UPDATE template header, UPDATE header menu dropdown on mobile (adds span with triangle to expand the submenu when clicked), UPDATE header navbar style, id/class names, comments

--- js/header.js
+++ js/header.js
@@ -1,4 +1,4 @@
-const menuDropdown = document.getElementById("menu-mobile-dropdown");
+const menuDropdown = document.getElementById("menu-mobile-dropdown-container");
 const menuDropdownBackground = document.getElementById("menu-dropdown-background");
 const menuDropdownTopDefaultValue = getComputedStyle(menuDropdown).top;
 
@@ -15,8 +15,8 @@ const menuDropdownButton = document.getElementById("menu-mobile-btn");
 menuDropdownButton.addEventListener("click", () => {
   if (getComputedStyle(menuDropdown).visibility === "hidden") {
     menuDropdown.style.visibility = "visible";
-    menuDropdown.style.opacity = 1;
-    menuDropdown.style.top = "52px";
+    menuDropdown.style.opacity = 0.9;
+    menuDropdown.style.top = "56px";
     menuDropdownBackground.style.display = "block";
   } else {
     menuDropdownHide();
@@ -24,7 +24,7 @@ menuDropdownButton.addEventListener("click", () => {
 });
 
 // Click on the mobile menu navbar links to hide menu navbar
-const menuDropdownItems = document.getElementsByClassName("menu-dropdown-a");
+const menuDropdownItems = document.getElementsByClassName("dropdown-element-a");
 for (let i = 0; i < menuDropdownItems.length; i++) {
   const dropdownItem = menuDropdownItems[i];
   dropdownItem.addEventListener("click", menuDropdownHide);
@@ -32,3 +32,30 @@ for (let i = 0; i < menuDropdownItems.length; i++) {
 
 // Click anywhere (background, except mobile menu navbar button and mobile menu navbar links) to hide mobile menu navbar (if mobile menu navbar is open)
 document.getElementById("menu-dropdown-background").addEventListener("click", menuDropdownHide);
+
+// Click on the triangle in mobile dropdown menu to expand the submenu
+const triangleMobileContainer = Array.from(
+  document.getElementsByClassName("dropdown-element-span")
+);
+triangleMobileContainer.forEach((triangle, whichSubmenu) => {
+  triangle.addEventListener("click", () => {
+    // Displays a submenu depending on which triangle was clicked
+    switch (whichSubmenu) {
+      // Unit Coverters
+      case 0:
+        break;
+
+      // Online calcualtors
+      case 1:
+        break;
+
+      // Text Content Tools
+      case 2:
+        break;
+
+      // Other Tools
+      case 3:
+        break;
+    }
+  });
+});
