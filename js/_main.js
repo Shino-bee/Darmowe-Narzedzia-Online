6a2783475c83bd33dae5f55b182bd5cd3bbf1582
Michał Podowski <michal.podowski@gmail.com>
2024-03-17 23:53:58 +0100

    FIX age calculator inputs width (on mobile) and box-shadow, MERGE error pages styles to one css file error_pages.css (DRY rule), CHANGED .html to .shtml header and footer templates extensions, ADDED new css variable --box-shadow-inputs

--- js/_main.js
+++ js/_main.js
@@ -1,15 +1,15 @@
-// Load <header> navbar template from file (template_header.html)
+// Load <header> navbar template from file (template_header.shtml)
 const xhrHeader = new XMLHttpRequest();
 xhrHeader.onload = function () {
   document.getElementById("header").innerHTML = this.response;
 };
-xhrHeader.open("GET", "../../pages/_templates/template_header.html", false);
+xhrHeader.open("GET", "../../pages/_templates/template_header.shtml", false);
 xhrHeader.send();
 
-// Load <footer> template from file (template_footer.html)
+// Load <footer> template from file (template_footer.shtml)
 const xhrFooter = new XMLHttpRequest();
 xhrFooter.onload = function () {
   document.getElementById("footer").innerHTML = this.response;
 };
-xhrFooter.open("GET", "../../pages/_templates/template_footer.html", false);
+xhrFooter.open("GET", "../../pages/_templates/template_footer.shtml", false);
 xhrFooter.send();
